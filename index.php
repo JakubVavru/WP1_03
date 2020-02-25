24ec3e760f00abd0fa646ef108e55ea1d8378bff
JakubVavru <JakubVavru@users.noreply.github.com>
2020-02-25 08:27:01 +0100

    Matematické operátory
    Jupí

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$x = 24;
+$y = 2;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>Součet: <?= $x ?> + <?= $y ?> = <?= $x + $y ?></p>
+
+<p>Rozdíl: <?= $x ?> - <?= $y ?> = <?= $x - $y ?></p>
+
+<p>Součin: <?= $x ?> x <?= $y ?> = <?= $x * $y ?></p>
+
+<p>Podíl: <?= $x ?> : <?= $y ?> = <?= $x / $y ?></p>
+
+</body>
+</html>
